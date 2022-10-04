3fc9e751d5f3e24c47e31a3f1296abb39f7f7a1a
Phat <phatdroid94@gmail.com>
2022-10-04 18:58:09 +0700

    Initial commit

---
+++ bin/dart_fundamental29092022.dart
@@ -0,0 +1,20 @@
+void main(List<String> arguments) {
+
+  // 1: Kiểu dữ liệu
+  // Kiểu số
+  // num weight = 75.5;
+  // Kiểu chuỗi
+  // String name = "Phat";
+  // Kiểu luận lý
+  // bool isMale = true;
+  // Kiểu run
+  // String unicode1 = "\u01FF";
+  // print(unicode1);
+  // Runes runes = Runes(
+  //    '\u{2651} \u{2652} \u{2653} \u{2654}'
+  // );
+  // print(String.fromCharCodes(runes));
+  dynamic a = 10;
+  a = "10";
+  print(a);
+}
